7d1e3061e4152400432de30ec6399ef0b4abaaba
mertonwagner <107504603+mertonwagner@users.noreply.github.com>
2022-06-15 11:17:39 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,50 @@
+let degrees = 0
+basic.forever(function () {
+    degrees = input.compassHeading()
+    if (degrees < 45) {
+        basic.showString("N")
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    } else if (degrees < 135) {
+        basic.showString("W")
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    } else if (degrees < 225) {
+        basic.showString("S")
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    } else if (degrees < 315) {
+        basic.showString("E")
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    } else {
+        basic.showString("N")
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    }
+})
